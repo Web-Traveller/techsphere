c2cd4ac3771bf87b7dfc704e60c57ff0cbdd830e
AMOLJADHAV9 <amolj9238@gmail.com>
2025-01-09 01:49:34 +0530

    timezone

--- techsphere/settings.py
+++ techsphere/settings.py
@@ -112,11 +112,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/kolkata'
+TIME_ZONE = 'Asia/Kolkata'  # Correct timezone setting for IST
+USE_TZ = True
 
 USE_I18N = True
 
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
